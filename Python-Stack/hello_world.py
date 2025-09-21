67e2c304eb9b1096ba3768c9b0b70a25f644d142
Khaled <Khaled031203@gmial.com>
2025-09-21 13:24:41 +0300

    python stack

---
+++ Python-Stack/hello_world.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+
+name = "Khaled"
+print("Hello",name)
+print("Hello " + name)
+
+fav = 10
+print("Hello", fav)
+#print("Hello " + fav) #it will make an error because you want to cast the int to (str)
+
+print("Hello " + str(fav))
+
+first = "Kabab"
+second = "Falafel"
+print("I love to eat {} and {} ".format(first, second))
+print(f"I love to eat {first} and {second} ")
+
+
+
